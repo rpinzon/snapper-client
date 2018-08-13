5a6b27c8432ad279b0f967650069f7775c3d4446
Renan Tomazoni Pinzon <rpinzon@gmail.com>
2018-08-12 23:58:34 -0300

    logging, read data from file and call the rest

--- src/main/java/org/rpinzon/snapper/StartApp.java
+++ src/main/java/org/rpinzon/snapper/StartApp.java
@@ -1,13 +1,21 @@
 package org.rpinzon.snapper;
 
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.rpinzon.snapper.model.Data;
 import org.rpinzon.snapper.util.Configs;
-import org.springframework.boot.CommandLineRunner;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.SimpleCommandLinePropertySource;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 /**
  * Created by Renan T. Pinzon on 02/08/18.
@@ -15,32 +23,46 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 public class StartApp {
 
-    private static final String PATH = "path";
+    private static final Logger LOG = LoggerFactory.getLogger(StartApp.class);
+
+    private static final String FILENAME = "filename";
 
     private static final String SERVER = "server";
 
     private static final String PORT = "port";
 
     public static void main(String[] args) {
-        SimpleCommandLinePropertySource line = new SimpleCommandLinePropertySource(args);
-        String path = path(line);
-        String uri = uri(line);
+        SimpleCommandLinePropertySource commandLine = new SimpleCommandLinePropertySource(args);
+        String filename = filename(commandLine);
+        String uri = uri(commandLine);
         SpringApplication.run(StartApp.class, args);
-        System.out.println("uri = " + uri);
-        System.out.println("path = " + path);
+        ObjectMapper mapper = new ObjectMapper();
         RestTemplate restTemplate = new RestTemplate();
-        Data data = new Data();
-        data.setPk(1);
-        data.setScore(200);
-        restTemplate.postForEntity(uri, data, Data.class);
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(filename))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                try {
+                    Data data = mapper.readValue(line, Data.class);
+                    restTemplate.postForEntity(uri, data, Data.class);
+                } catch (JsonParseException | JsonMappingException e) {
+                    LOG.error("Invalid line content!! Skipping to the next line!", e);
+                }
+            }
+        } catch (IOException e) {
+            LOG.error("Error attempting to read file " + filename, e);
+        } catch (RestClientException e) {
+            LOG.error("Error attempting to call the service at " + uri, e);
+        }
+
     }
 
-    private static String path(SimpleCommandLinePropertySource line) {
-        if (!line.containsProperty(PATH)) {
-            System.out.println("Invalid parameters!! Missing path to watch for files!");
+    private static String filename(SimpleCommandLinePropertySource line) {
+        if (!line.containsProperty(FILENAME)) {
+            LOG.error("Invalid parameters!! Missing filename!");
             System.exit(1);
         }
-        return line.getProperty(PATH);
+        return line.getProperty(FILENAME);
     }
 
     private static String uri(SimpleCommandLinePropertySource line) {
